d2be7da1ec197a0ffa397c96829f60b33d419621
oikawa-ryo0623 <r_oikawa@dpt-inc.co.jp>
2020-08-06 14:14:19 +0900

    pull

--- app/app.py
+++ app/app.py
@@ -151,14 +151,14 @@ def create_question_post():
     create_title_id = request.form["create_title_id"]
     create_category_id = request.form["create_category_id"]
     create_detail_id = request.form["create_detail_id"]
-    db.regist_question(create_title_id,create_category_id,create_detail_id)
+    db.regist_question(create_title_id,create_category_id,create_detail_id,'1')
 
     page = request.args.get(get_page_parameter(), type=int, default=1)
     all_questions = db.extract_all_questions()
     all_question = all_questions[(page - 1)*20: page*20]
     pagiantion = Pagination(page=page, total=len(all_questions), search=search, per_page=20, record_name='all_question', css_framework='bootstrap4')
     return render_template("index.html",all_question=all_question,pagination=pagiantion)
-    
+
 @app.route("/my_page")
 def my_page():
 
