4b84a038931d876c611a0c6336709e022802fed0
K.Kono <k_kono@dpt-inc.co.jp>
2020-08-07 11:11:19 +0900

    create_db.pyを更新しました。

--- models/create_db.py
+++ models/create_db.py
@@ -18,10 +18,10 @@ cursor.execute("USE test_db")
 cursor.execute("""
                 CREATE TABLE IF NOT EXISTS question(
                 id          INT UNSIGNED  AUTO_INCREMENT PRIMARY KEY,
-                title       VARCHAR(30)   NOT NULL,
-                category    VARCHAR(30)   NOT NULL,
+                title       VARCHAR(60)   NOT NULL,
+                category    VARCHAR(60)   NOT NULL,
                 regist_date DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                text        VARCHAR(3000) NOT NULL
+                text        VARCHAR(6000) NOT NULL
                 );
                 """)
 
@@ -29,7 +29,7 @@ cursor.execute("""
                 CREATE TABLE IF NOT EXISTS answer(
                 id          INT UNSIGNED  AUTO_INCREMENT PRIMARY KEY,
                 regist_date DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                text        VARCHAR(3000) NOT NULL,
+                text        VARCHAR(6000) NOT NULL,
                 q_id        INT UNSIGNED  NOT NULL,
                 FOREIGN KEY (q_id) REFERENCES question(id)
                 );
@@ -37,24 +37,24 @@ cursor.execute("""
 
 cursor.execute("""
                 CREATE TABLE IF NOT EXISTS user(
-                user_id       VARCHAR(30)   PRIMARY KEY,
-                user_password VARCHAR(30)   NOT NULL,
-                user_name     VARCHAR(30)   NOT NULL,
-                profile       VARCHAR(3000),
+                user_id       VARCHAR(60)   PRIMARY KEY,
+                user_password VARCHAR(60)   NOT NULL,
+                user_name     VARCHAR(60)   NOT NULL,
+                profile       VARCHAR(6000),
                 sex           CHAR(1) NOT NULL
                 );
                 """)
 
 cursor.execute("""
                 CREATE TABLE IF NOT EXISTS user_question(
-                user_id VARCHAR(30) REFERENCES user(user_id),
+                user_id VARCHAR(60) REFERENCES user(user_id),
                 q_id    INT         REFERENCES question(id)
                 );
                 """)
 
 cursor.execute("""
                 CREATE TABLE IF NOT EXISTS user_answer(
-                user_id VARCHAR(30) REFERENCES user(user_id),
+                user_id VARCHAR(60) REFERENCES user(user_id),
                 a_id    INT         REFERENCES answer(id)
                 );
                 """)
